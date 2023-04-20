7bfdcae711b5b8ff4c66ed3e94ae865b7769acf7
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-20 10:19:50 -0300

    Footer Component

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MenuItem from "./menuItem";
 import MenuTree from "./menuTree";
 
+
 export default props => (
     <ul className="sidebar-menu">
         <MenuItem path="#/" label="Dashboard" icon="dashboard" />

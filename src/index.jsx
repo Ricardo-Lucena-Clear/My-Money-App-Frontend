4c281b43a194107c13122933a87deef3d33a97bf
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-20 12:06:53 -0300

    Integrating Dashboard with Redux (Part 2)

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import App from "./main/app.jsx";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import promise from "redux-promise";
 import { Provider } from "react-redux"
 import reducers from "./main/reducers.js"
 
-const store = createStore(reducers)
+const store = applyMiddleware(promise)(createStore)(reducers)
 ReactDOM.render(
     <Provider store={store}>
         <App />

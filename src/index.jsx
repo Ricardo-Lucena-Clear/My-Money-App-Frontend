dc6940b578f671ba4b06a8e33340c3878f3a3dfa
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-24 10:34:31 -0300

    Payment Cycle Inclusion Improvements

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom"
 import App from "./main/app.jsx";
 import { applyMiddleware, createStore } from "redux";
 import promise from "redux-promise";
+import multi from "redux-multi"
+import thunk from "redux-thunk"
 import { Provider } from "react-redux"
 import reducers from "./main/reducers.js"
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
       && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App />
